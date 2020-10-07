9b206fff107c4b878a7ac587712913628504aaed
Felix <neostyxde@gmail.com>
2020-10-07 21:35:13 +0200

    commit

--- lib/main.py
+++ lib/main.py
@@ -4,9 +4,30 @@ from bs4 import BeautifulSoup
 url = "https://app.dsbcontrol.de/data/9c9bcbc1-132d-442c-bf82-52494b33d947/d08b92ff-6c3e-43e8-8457-6d24f4cb305a/subst_001.htm"
 r = rq.get(url)
 bs = BeautifulSoup(r.text, "lxml")
-found = bs.findAll("span")[3:]
+found = bs.findAll("td")[3:]
+
+currentItem = ""
+finalList = []
+currentList = []
+
 for item in found:
-    try:
-        print(str(item).replace('<span style="color: #010101">', "").replace("</span>", ""))
-    except:
-        print("ERROR")
+    aboutToPrint = str(item).split(">")
+    aboutToPrint = aboutToPrint[1].replace("</span>", "").replace("</td","").replace('<span style="color: #010101"',"").replace("<b","")
+    if aboutToPrint == " " or aboutToPrint == "---" or aboutToPrint == "" or aboutToPrint == "-----":
+        pass
+    
+    # elif aboutToPrint.startswith("0"):
+        # pass
+
+    else:
+        if aboutToPrint.startswith("0") or aboutToPrint.startswith("Q") or aboutToPrint == ("EF"):
+            finalList.append(currentList)
+            currentList = []
+    
+        currentItem += aboutToPrint
+        currentList.append(aboutToPrint)
+
+# print(finalList)
+
+for item in finalList:
+    print(item)
